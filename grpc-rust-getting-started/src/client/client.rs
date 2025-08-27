72341046a22037d2aa31a595b07fa6aa81de048a
yNOtOmosh <onyango.omondi.tony@gmail.com>
2025-08-27 12:21:48 +0300

    creating the client, calling the service methods

--- grpc-rust-getting-started/src/client/client.rs
+++ grpc-rust-getting-started/src/client/client.rs
@@ -2,17 +2,36 @@ use tonic::Request;
 use tonic::transport::{Endpoint}; 
 use protobuf::proto;
 
-// /////////////////////////////////////////////////////////////////////////
-// Codelab Hint: Bring the generated code into scope.
-// /////////////////////////////////////////////////////////////////////////
+mod grpc_pb {
+    grpc::include_generated_photo!("generated", "routeguide");
+}
+
+use grpc_pb::{
+    route_guide_client::RouteGuideClient,
+    Point,
+};
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
-    ///////////////////////////////////////////////////////////////////////////
-	// Codelab Hint: Logic for your gRPC Client will be added here.
-	//
-	// Steps include:
-	//  -   Create a connection to the gRPC server using RouteGuideClient::connect(...).
-	//  -   Call service methods on the client to interact with the server.
-	///////////////////////////////////////////////////////////////////////////
+    //ncreate end point to connect to
+    let endpoint = Endpoint::new("http://[::1]:10000")?;
+    let channel = endpoint.connect().await?;
+
+    // create new client
+    let mut client = RouteGuideClient::new(channel);
+
+    println!("*** SIMPLE RPC ***");
+    let point = proto!(Point{
+        latitude: 409_146_138,
+        longitude: -746_188_906
+    });
+    let response = client
+        .get_feature(Request::new(point))
+        .await?.into_inner();
+
+    println!("Response = Name = \"{}\", Latitude = {}, Longitude = {}",
+        response.name(),
+        response.location().latitude(),
+        response.location().longitude());
+    Ok(())
 }
